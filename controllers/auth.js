6fa2d9a8a83e2eba73256b80739869bd564d19cb
usman zafar <musman.zafar@systemsltd.com>
2023-08-08 15:16:51 +0500

    initial push

--- controllers/auth.js
+++ controllers/auth.js
@@ -111,57 +111,57 @@
                      email: req.body.email,
                      name: req.body.name,
              }
-        })
-        .then(() => {
-            res.status(200).json({
-                message: 'User Updated',
-            })
-        })
-    })
-    
-    .catch(err => {
-        if(!err.statusCode) {
-            err.statusCode = 500;
-        }
-        next(err);
-    })
-}
-
-exports.deleteUser = (req, res, next) => {
-    const id = req.body._id;
-    // const errors = validationResult(req);
-    // if (!errors.isEmpty()) {
-    //     const error = new Error('Validation failed.');
-    //     error.statusCode = 422;
-    //     error.data = errors.array();
-    //     throw error;
-    // }
-    let loadedUser;
-    User.findOne({
-        _id: id
-    })
-    .then(user => {
-        if(!user) {
-            const error = new Error('A user with this ID doesnot exist.');
-            error.statusCode = 401;
-            throw error;
-        }
-        loadedUser = user;
-        User.deleteOne(
-            {
-                _id: loadedUser._id
-            })
-        .then(() => {
-            res.status(200).json({
-                message: 'User Deleted',
-            })
-        })
-    })
-    
-    .catch(err => {
-        if(!err.statusCode) {
-            err.statusCode = 500;
-        }
-        next(err);
-    })
-}
+         })
+         .then(() => {
+             res.status(200).json({
+                 message: 'User Updated',
+             })
+         })
+     })
+     
+     .catch(err => {
+         if(!err.statusCode) {
+             err.statusCode = 500;
+         }
+         next(err);
+     })
+ }
+ 
+ exports.deleteUser = (req, res, next) => {
+     const id = req.body._id;
+     // const errors = validationResult(req);
+     // if (!errors.isEmpty()) {
+     //     const error = new Error('Validation failed.');
+     //     error.statusCode = 422;
+     //     error.data = errors.array();
+     //     throw error;
+     // }
+     let loadedUser;
+     User.findOne({
+         _id: id
+     })
+     .then(user => {
+         if(!user) {
+             const error = new Error('A user with this ID doesnot exist.');
+             error.statusCode = 401;
+             throw error;
+         }
+         loadedUser = user;
+         User.deleteOne(
+             {
+                 _id: loadedUser._id
+             })
+         .then(() => {
+             res.status(200).json({
+                 message: 'User Deleted',
+             })
+         })
+     })
+     
+     .catch(err => {
+         if(!err.statusCode) {
+             err.statusCode = 500;
+         }
+         next(err);
+     })
+ }
